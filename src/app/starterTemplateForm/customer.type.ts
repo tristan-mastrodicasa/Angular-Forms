75000313524ce8a205dcad619a52d4c2a22c5fd3
Tristan <ghoststeam217@gmail.com>
2020-11-25 16:03:13 -0500

    Added new fields to Starter Template Form

---
+++ src/app/starterTemplateForm/customer.type.ts
@@ -0,0 +1,11 @@
+export type Customer = {
+  firstName: string;
+  lastName: string;
+  address: string;
+  city: string;
+  province: string;
+  country: string;
+  postalCode: string;
+  phoneNumber: string;
+  email: string;
+};
--- src/app/starterTemplateForm/starterTemplateForm.component.ts
+++ src/app/starterTemplateForm/starterTemplateForm.component.ts
@@ -1,23 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Customer } from './customer.type';
+
 @Component({
     selector: 'starter-template-form',
-    templateUrl: './starterTemplateForm.component.html'
+    templateUrl: './starterTemplateForm.component.html',
+    styleUrls: ['./starterTemplateForm.component.css'],
 })
 export class StarterTemplateFormComponent implements OnInit {
-    customer: any;
+    customer: Customer;
     message: string;
 
     constructor() { }
 
-    ngOnInit() { 
+    ngOnInit() {
         this.customer = {
-            firstName: 'Fred Template'
+            firstName: 'John',
+            lastName: 'Doe',
+            address: '123 Main',
+            city: 'Calgary',
+            province: 'Alberta',
+            country: 'Canada',
+            postalCode: 'A1A 1A1',
+            phoneNumber: '111-222-3333',
+            email: 'jdoe@email.com'
         };
     }
 
     onSubmit() {
-        this.message = 'You typed: ' + this.customer.firstName;
+      this.message = '<b>First Name:</b> ' + this.customer.firstName + '<br>';
+      this.message += '<b>Last Name:</b> ' + this.customer.lastName + '<br>';
+      this.message += '<b>Address:</b> ' + this.customer.address + '<br>';
+      this.message += '<b>City:</b> ' + this.customer.city + '<br>';
+      this.message += '<b>Province:</b> ' + this.customer.province + '<br>';
+      this.message += '<b>Country:</b> ' + this.customer.country + '<br>';
+      this.message += '<b>Postal Code:</b> ' + this.customer.postalCode + '<br>';
+      this.message += '<b>Phone Number:</b> ' + this.customer.phoneNumber + '<br>';
+      this.message += '<b>Email:</b> ' + this.customer.email + '<br>';
     }
 
 }
